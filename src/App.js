625ec8805fdb6795f9031b8f9e6146328494f3d5
deepen77 <przemek.starostka@yahoo.com>
2022-04-23 22:49:55 +0200

    ui components implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,86 @@
-import './App.css';
+import React, {useState} from "react";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { Box, TextField, MenuItem, Button, Typography, Select, InputLabel, FormControl, FormHelperText } from "@mui/material";
+import moment from "moment";
+
+
 
 function App() {
+
+  const [dateOfBirth, setDateOfBirth] = useState(null);
+  const [gender, setGender] = useState("");
+  
+
+
+
+  const sendForm = (e) => {
+    e.preventDefault();
+    if (dateOfBirth === "" || gender === "") {
+      console.log("not validated");
+      return;
+    }
+    console.log(dateOfBirth);
+    console.log(moment(dateOfBirth).format("YYYY-MM-DD"));
+    setDateOfBirth(null);
+    setGender("");
+
+  };
+
+
   return (
-    <div className="App">
-        Test
-    </div>
+    <>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="column"
+        marginTop={10}
+        padding={5}
+      >
+        <Typography variant="h3">PESEL-GENERATOR</Typography>
+        <Box margin="30px auto 0 auto">
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <DatePicker
+              label="Date Of Birth"
+              value={dateOfBirth}
+              onChange={(newValue) => setDateOfBirth(newValue)}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  helperText={params?.inputProps?.placeholder}
+                />
+              )}
+            />
+          </LocalizationProvider>
+        </Box>
+        <Box width="250px" textAlign="center" margin="30px auto">
+          <FormControl sx={{ m: 1, minWidth: 120 }}>
+            <InputLabel id="demo-simple-select-helper-label">Gender</InputLabel>
+            <Select
+              labelId="demo-simple-select-helper-label"
+              id="demo-simple-select-helper"
+              value={gender}
+              label="Gender"
+              onChange={(e) => setGender(e.target.value)}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={"W"}>Woman</MenuItem>
+              <MenuItem value={"M"}>Man</MenuItem>
+            </Select>
+            <FormHelperText>Please select gender</FormHelperText>
+          </FormControl>
+        </Box>
+        <Box marginTop={3}>
+          <Button type="submit" variant="contained" onClick={sendForm}>
+            GENERATE
+          </Button>
+        </Box>
+      </Box>
+    </>
   );
 }
 
